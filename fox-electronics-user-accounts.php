e8edf6f9c4b2cfd80ec73df4fc0716ad0ec70462
Michael Wender <michael@michaelwender.com>
2019-01-04 11:37:16 -0500

    First commit

---
+++ fox-electronics-user-accounts.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Fox Electronics User Accounts
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     Extends user accounts for integration with Fox Electronics business rules and SalesForce integration.
+ * Author:          Michael Wender
+ * Author URI:      https://michaelwender.com
+ * Text Domain:     fox-electronics-user-accounts
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Fox_Electronics_User_Accounts
+ */
+
+// Your code starts here.
